e08866af4389b16f3dd91ebd9ac9dda841e95538
Anders Poulsen <and.poulsen@gmail.com>
2018-03-14 11:35:49 +0100

    Updating script to add values and types automatically and print out results from db

--- index.php
+++ index.php
@@ -1,37 +1,77 @@
 <html>
+<?php
+    $servername = "localhost";
+    $username = "test";
+    $password = "test";
+ ?>
     <body>
-        <h1>Test</h1>
+        <h1>Sending and saving values test</h1>
+
+        <h2>Received values</h2>
         <?php
             parse_str($_SERVER['QUERY_STRING'], $query);
 
-
             foreach($query as $key=>$value){
-                echo "$key => $value<br/>";
+                echo "You sent: $key = $value<br/>";
             }
         ?>
 
         <?php 
-            $servername = "localhost";
-            $username = "test";
-            $password = "test";
+            function addValueToDb($type, $value){
+                $servername = "localhost";
+                $username = "test";
+                $password = "test";
 
-            try{
-                $conn = new PDO('mysql:host=localhost;dbname=sys', $username, $password);
+                try{
+                    $conn = new PDO("mysql:host=$servername;dbname=sys", $username, $password);
 
-                // Insert new query string parameters not seen before
-                foreach($query as $key=>$value){
+                    // Insert new query string parameter if has not been seen before
                     $insertsql = "INSERT INTO sys.queryparams (queryparam) 
-                    SELECT '$key' 
-                    WHERE NOT EXISTS (SELECT * FROM sys.queryparams WHERE queryparam='$key');";
+                                    SELECT '$type' 
+                                    WHERE NOT EXISTS (SELECT * FROM sys.queryparams WHERE queryparam='$type');
+
+                                  INSERT INTO sys.values (value, type) 
+                                    SELECT '$value', id
+                                    FROM sys.queryparams 
+                                    WHERE queryparam='$type';
+                                  ";
                     $conn->query($insertsql);
+
+                }catch(PDOException $e){
+                    print "Error!: " . $e->getMessage() . "<br/>";
+                    die();
+                }
+            }
+        ?>
+
+        <h2>Known types in DB</h2>
+        <?php 
+            try{
+                foreach($query as $key=>$value){
+                    addValueToDb($key, $value);
                 }
 
-                
+                $conn = new PDO("mysql:host=$servername;dbname=sys", $username, $password);
                 $sql = "SELECT queryparam FROM queryparams";
                 foreach($conn->query($sql) as $row) {
                     echo $row['queryparam'] . "<br />";
                 }
             }catch(PDOException $e){
+                print "Error!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        ?>
+
+        <h2>Values in DB</h2>
+        <?php
+            try{
+                $conn = new PDO("mysql:host=$servername;dbname=sys", $username, $password);
+                $sql = "SELECT * FROM sys.values v INNER JOIN sys.queryparams q ON v.type=q.id ORDER BY v.id DESC";
+                foreach($conn->query($sql) as $row){
+                    echo $row['queryparam'] . " = " . $row['value'] . "<br />";
+                }
+
+            }catch(PDOExceptoin $e){
                 print "Error!: " . $e->getMessage() . "<br/>";
                 die();
             }
